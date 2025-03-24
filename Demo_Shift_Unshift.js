5b34478fc7dbf7504216857b3bd1f4e106c69b6e
Balrampatid <balram421021@gmail.com>
2025-03-24 17:22:47 +0530

    Add files via upload

---
+++ Demo_Shift_Unshift.js
@@ -0,0 +1,10 @@
+var a=["ram","shyam","mohan","karan","deepak"];
+
+// shift functin is use for delete first elemnt 
+console.log(a+"<br><br>");
+a.shift();// delete First element
+console.log(a+"<br><br>");
+a.unshift("rahul");//add first element in arrya
+console.log(a+"<br><br>");
+a.unshift("karan");
+console.log(a+"<br><br>");
